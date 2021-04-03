b709dc1bb4b867b6bacb126988db8b6460e8661b
Greg Hawthorne <ghawthorne@me.com>
2021-04-03 08:05:12 +0200

    set up orders screen

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/providers/cart.dart';
 import './cart.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class OrderItem {
   final String id;
@@ -23,8 +25,27 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(0, OrderItem(id: DateTime.now().toString(),
+  Future <void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(
+        'flutterapp-1a145-default-rtdb.firebaseio.com', '/products.json');
+    final timeStamp = DateTime.now();
+    final response = await http.post(url, body: json.encode({
+    'amount': total,
+    'dateTime': timeStamp.toIso8601String(),
+      'products': cartProducts.map((cp) =>
+      {
+        'id': cp.id,
+        'title': cp.title,
+        'quantity': cp.quantity,
+        'price': cp.price,
+      })
+          .toList(),
+
+    }),);
+
+    _orders.insert(0,
+    OrderItem(
+      id: json.decode(response.body)['name'],
         amount: total,
         products: cartProducts,
         dateTime: DateTime.now(),
